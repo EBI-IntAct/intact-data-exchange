4fcc9d7c873f9daf530968b78efc680e58831020
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-08-07 17:15:32 +0000

    Added ConverterContext that allows to access converter configuration from anywhere (uses ThreadLocal). Added configuration options for Interactor (JIRA IDE-15) Now one can: - remove the sequence of an interactor - no export aliases - filter the xrefs by database and qualifier

--- psimi/psixml/intact-psixml-converters/src/main/java/uk/ac/ebi/intact/dataexchange/psimi/xml/converter/util/PsiConverterUtils.java
+++ psimi/psixml/intact-psixml-converters/src/main/java/uk/ac/ebi/intact/dataexchange/psimi/xml/converter/util/PsiConverterUtils.java
@@ -17,18 +17,25 @@ package uk.ac.ebi.intact.dataexchange.psimi.xml.converter.util;
 
 import psidev.psi.mi.xml.model.*;
 import psidev.psi.mi.xml.model.Xref;
+import uk.ac.ebi.intact.dataexchange.psimi.xml.converter.InteractorConverterConfig;
+import uk.ac.ebi.intact.dataexchange.psimi.xml.converter.ConverterContext;
+import uk.ac.ebi.intact.dataexchange.psimi.xml.converter.shared.AliasConverter;
+import uk.ac.ebi.intact.dataexchange.psimi.xml.converter.shared.AnnotationConverter;
 import uk.ac.ebi.intact.dataexchange.psimi.xml.converter.shared.CvObjectConverter;
 import uk.ac.ebi.intact.dataexchange.psimi.xml.converter.shared.XrefConverter;
-import uk.ac.ebi.intact.dataexchange.psimi.xml.converter.shared.AnnotationConverter;
+import uk.ac.ebi.intact.model.Alias;
 import uk.ac.ebi.intact.model.*;
+import uk.ac.ebi.intact.model.Interaction;
+import uk.ac.ebi.intact.model.Interactor;
+import uk.ac.ebi.intact.model.util.AnnotatedObjectUtils;
 
 import java.util.Collection;
 import java.util.HashSet;
 
 /**
- * TODO comment this
+ * PSI converter utilities.
  *
- * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk), Bruno Aranda (baranda@ebi.ac.uk)
  * @version $Id$
  */
 public class PsiConverterUtils {
@@ -38,214 +45,262 @@ public class PsiConverterUtils {
     private PsiConverterUtils() {
     }
 
-    public static void populate(AnnotatedObject<?, ?> annotatedObject, Object objectToPopulate) {
-        if (objectToPopulate instanceof HasId) {
-            populateId((HasId) objectToPopulate);
+    public static void populate( AnnotatedObject<?, ?> annotatedObject, Object objectToPopulate ) {
+        if ( objectToPopulate instanceof HasId ) {
+            populateId( ( HasId ) objectToPopulate );
         }
 
-        if (objectToPopulate instanceof NamesContainer) {
-            populateNames(annotatedObject, (NamesContainer) objectToPopulate);
+        if ( objectToPopulate instanceof NamesContainer ) {
+            populateNames( annotatedObject, ( NamesContainer ) objectToPopulate );
         }
 
-        if (objectToPopulate instanceof XrefContainer) {
-            populateXref(annotatedObject, (XrefContainer) objectToPopulate);
+        if ( objectToPopulate instanceof XrefContainer ) {
+            populateXref( annotatedObject, ( XrefContainer ) objectToPopulate );
         }
 
-        if (objectToPopulate instanceof AttributeContainer) {
-            populateAttributes(annotatedObject, (AttributeContainer) objectToPopulate);
+        if ( objectToPopulate instanceof AttributeContainer ) {
+            populateAttributes( annotatedObject, ( AttributeContainer ) objectToPopulate );
         }
     }
 
-    private static void populateNames(AnnotatedObject<?, ?> annotatedObject, NamesContainer namesContainer) {
+    private static void populateNames( AnnotatedObject<?, ?> annotatedObject, NamesContainer namesContainer ) {
         Names names = namesContainer.getNames();
 
-        if (names == null) {
+        if ( names == null ) {
             names = new Names();
         }
 
         String shortLabel = annotatedObject.getShortLabel();
         String fullName = annotatedObject.getFullName();
 
-        names.setShortLabel(shortLabel);
-        names.setFullName(fullName);
+        names.setShortLabel( shortLabel );
+        names.setFullName( fullName );
 
-        namesContainer.setNames(names);
+        if ( ConverterContext.getInstance().getInteractorConfig().isExcludeInteractorAliases() ) {
+            AliasConverter aliasConverter = new AliasConverter( annotatedObject.getOwner(),
+                                                                AnnotatedObjectUtils.getAliasClassType( annotatedObject.getClass() ) );
+            for ( Alias alias : annotatedObject.getAliases() ) {
+                names.getAliases().add( aliasConverter.intactToPsi( alias ) );
+            }
+        }
+
+        namesContainer.setNames( names );
     }
 
-    private static void populateXref(AnnotatedObject<?, ?> annotatedObject, XrefContainer xrefContainer) {
-        if (annotatedObject.getXrefs().isEmpty()) {
+    private static void populateXref( AnnotatedObject<?, ?> annotatedObject, XrefContainer xrefContainer ) {
+        if ( annotatedObject.getXrefs().isEmpty() ) {
             return;
         }
 
         Xref xref = xrefContainer.getXref();
 
-        if (xref == null) {
+        if ( xref == null ) {
             xref = new Xref();
         }
 
-        Collection<DbReference> dbRefs = toDbReferences(annotatedObject.getXrefs());
+        Collection<DbReference> dbRefs = toDbReferences( annotatedObject, annotatedObject.getXrefs() );
 
         // normally the primary reference is the identity reference, but for bibliographic references
         // it is the primary-reference and it does not contain secondary refs
-        if (xrefContainer instanceof Bibref) {
-            DbReference primaryRef = getPrimaryReference(dbRefs);
-            xref.setPrimaryRef(primaryRef);
+        if ( xrefContainer instanceof Bibref ) {
+            DbReference primaryRef = getPrimaryReference( dbRefs );
+            xref.setPrimaryRef( primaryRef );
         } else {
-            DbReference primaryRef = getIdentity(dbRefs);
-            xref.setPrimaryRef(primaryRef);
+            DbReference primaryRef = getIdentity( dbRefs );
+            xref.setPrimaryRef( primaryRef );
 
             // remove the primary ref and the bibref (primary-ref) in case of being an experiment
             // from the collection and add the rest as secondary refs
-            dbRefs.remove(primaryRef);
+            dbRefs.remove( primaryRef );
 
-            if (annotatedObject instanceof Experiment) {
-                dbRefs.remove(getPrimaryReference(dbRefs));
+            if ( annotatedObject instanceof Experiment ) {
+                dbRefs.remove( getPrimaryReference( dbRefs ) );
             }
 
-            for (DbReference secDbRef : dbRefs) {
-                if (!xref.getSecondaryRef().contains(secDbRef)) {
-                    xref.getSecondaryRef().add(secDbRef);
+            for ( DbReference secDbRef : dbRefs ) {
+                if ( !xref.getSecondaryRef().contains( secDbRef ) ) {
+                    xref.getSecondaryRef().add( secDbRef );
                 }
             }
         }
 
-
-        xrefContainer.setXref(xref);
+        xrefContainer.setXref( xref );
     }
 
-    private static int populateId(HasId hasIdElement) {
-        if (hasIdElement.getId() > 0) {
+    private static int populateId( HasId hasIdElement ) {
+        if ( hasIdElement.getId() > 0 ) {
             return hasIdElement.getId();
         }
-        
+
         int id = IdSequenceGenerator.getInstance().nextId();
-        hasIdElement.setId(id);
+        hasIdElement.setId( id );
 
         return id;
     }
 
-    private static void populateAttributes(AnnotatedObject<?, ?> annotatedObject, AttributeContainer attributeContainer) {
-        AnnotationConverter annotationConverter = new AnnotationConverter(annotatedObject.getOwner());
+    private static void populateAttributes( AnnotatedObject<?, ?> annotatedObject, AttributeContainer attributeContainer ) {
+        AnnotationConverter annotationConverter = new AnnotationConverter( annotatedObject.getOwner() );
 
-        for (Annotation annotation : annotatedObject.getAnnotations()) {
-            Attribute attribute = annotationConverter.intactToPsi(annotation);
-            attributeContainer.getAttributes().add(attribute);
+        for ( Annotation annotation : annotatedObject.getAnnotations() ) {
+            Attribute attribute = annotationConverter.intactToPsi( annotation );
+            attributeContainer.getAttributes().add( attribute );
         }
     }
 
-    public static CvType toCvType(CvObject cvObject, CvObjectConverter converter) {
-        if (cvObject == null) {
-            throw new NullPointerException("cvObject");
+    public static CvType toCvType( CvObject cvObject, CvObjectConverter converter ) {
+        if ( cvObject == null ) {
+            throw new NullPointerException( "cvObject" );
         }
 
-        CvType cvType = converter.intactToPsi(cvObject);
-        populate(cvObject, cvType);
+        CvType cvType = converter.intactToPsi( cvObject );
+        populate( cvObject, cvType );
 
         return cvType;
     }
 
     public static BiologicalRole createUnspecifiedBiologicalRole() {
-        return createUnspecifiedRole(BiologicalRole.class);
+        return createUnspecifiedRole( BiologicalRole.class );
     }
 
     public static ExperimentalRole createUnspecifiedExperimentalRole() {
-        return createUnspecifiedRole(ExperimentalRole.class);
+        return createUnspecifiedRole( ExperimentalRole.class );
     }
 
-    private static <T extends CvType> T createUnspecifiedRole(Class<T> roleClass) {
+    private static <T extends CvType> T createUnspecifiedRole( Class<T> roleClass ) {
         T role;
 
         try {
             role = roleClass.newInstance();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch ( Exception e ) {
+            throw new RuntimeException( e );
         }
 
         Names names = new Names();
-        names.setShortLabel(UNSPEFICIED_ROLE);
-        names.setFullName(UNSPEFICIED_ROLE);
+        names.setShortLabel( UNSPEFICIED_ROLE );
+        names.setFullName( UNSPEFICIED_ROLE );
 
-        role.setNames(names);
+        role.setNames( names );
 
         Xref xref = new Xref();
 
         String unspecifiedRoleMiRef = "MI:0499";
 
-        DbReference dbRef = new DbReference(unspecifiedRoleMiRef, CvDatabase.PSI_MI);
-        dbRef.setRefType(CvXrefQualifier.IDENTITY);
-        dbRef.setRefTypeAc(CvXrefQualifier.IDENTITY_MI_REF);
-        dbRef.setDbAc(CvDatabase.PSI_MI_MI_REF);
+        DbReference dbRef = new DbReference( unspecifiedRoleMiRef, CvDatabase.PSI_MI );
+        dbRef.setRefType( CvXrefQualifier.IDENTITY );
+        dbRef.setRefTypeAc( CvXrefQualifier.IDENTITY_MI_REF );
+        dbRef.setDbAc( CvDatabase.PSI_MI_MI_REF );
 
-        xref.setPrimaryRef(dbRef);
-        role.setXref(xref);
+        xref.setPrimaryRef( dbRef );
+        role.setXref( xref );
 
         return role;
     }
 
-    private static Collection<DbReference> toDbReferences(Collection<? extends uk.ac.ebi.intact.model.Xref> intactXrefs) {
-        Collection<DbReference> dbRefs = new HashSet<DbReference>(intactXrefs.size());
+    private static Collection<DbReference> toDbReferences( AnnotatedObject<?, ?> annotatedObject,
+                                                           Collection<? extends uk.ac.ebi.intact.model.Xref> intactXrefs ) {
 
-        for (uk.ac.ebi.intact.model.Xref intactXref : intactXrefs) {
-            XrefConverter xrefConverter = new XrefConverter(null, intactXref.getClass());
+        Collection<DbReference> dbRefs = new HashSet<DbReference>( intactXrefs.size() );
 
-            DbReference dbRef = xrefConverter.intactToPsi(intactXref);
-            dbRefs.add(dbRef);
+        for ( uk.ac.ebi.intact.model.Xref intactXref : intactXrefs ) {
+
+            if ( annotatedObject instanceof Interactor && !( annotatedObject instanceof Interaction ) ) {
+                // We have an interactor that is not an interaction.
+                if ( includeInteractorXref( intactXref ) ) {
+                    XrefConverter xrefConverter = new XrefConverter( null, intactXref.getClass() );
+                    DbReference dbRef = xrefConverter.intactToPsi( intactXref );
+                    dbRefs.add( dbRef );
+                }
+            } else {
+                XrefConverter xrefConverter = new XrefConverter( null, intactXref.getClass() );
+                DbReference dbRef = xrefConverter.intactToPsi( intactXref );
+                dbRefs.add( dbRef );
+            }
         }
 
         return dbRefs;
     }
 
     /**
+     * Figures out if the given Xref has to be exported given the InteractorConverterConfig available.
+     *
+     * @param intactXref the xref under inspection.
+     * @return true if the xref has to be converted, false otherwise.
+     */
+    private static boolean includeInteractorXref( uk.ac.ebi.intact.model.Xref intactXref ) {
+        CvDatabase db = intactXref.getCvDatabase();
+        CvXrefQualifier qualifier = intactXref.getCvXrefQualifier();
+        InteractorConverterConfig configInteractor = ConverterContext.getInstance().getInteractorConfig();
+
+        if ( configInteractor.isExcluded( qualifier ) || configInteractor.isExcluded( db ) ) {
+            return false;
+        }
+
+        if ( configInteractor.hasIncludesCvDatabase() ) {
+            if ( !configInteractor.isIncluded( db ) ) {
+                return false;
+            }
+        }
+
+        if ( configInteractor.hasIncludesCvXrefQualifier() ) {
+            if ( !configInteractor.isIncluded( qualifier ) ) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
      * The primary ref is the one that contains the qualifier 'identity', choosing the one with uniprot accession
      * if there is more than one "identities"
+     *
      * @param dbRefs
      * @return
      */
-    protected static DbReference getIdentity(Collection<DbReference> dbRefs) {
+    protected static DbReference getIdentity( Collection<DbReference> dbRefs ) {
         Collection<DbReference> identityRefs = new HashSet<DbReference>();
 
-        for (DbReference dbRef : dbRefs) {
-            if (dbRef.getRefTypeAc() != null && dbRef.getRefTypeAc().equals(CvXrefQualifier.IDENTITY_MI_REF)) {
+        for ( DbReference dbRef : dbRefs ) {
+            if ( dbRef.getRefTypeAc() != null && dbRef.getRefTypeAc().equals( CvXrefQualifier.IDENTITY_MI_REF ) ) {
 
-                if (dbRef.getDbAc() != null && dbRef.getDbAc().equals(CvDatabase.PSI_MI_MI_REF)) {
+                if ( dbRef.getDbAc() != null && dbRef.getDbAc().equals( CvDatabase.PSI_MI_MI_REF ) ) {
                     return dbRef;
                 }
 
-                identityRefs.add(dbRef);
+                identityRefs.add( dbRef );
             }
         }
 
-        if (!identityRefs.isEmpty()) {
+        if ( !identityRefs.isEmpty() ) {
             // return the one for uniprot, if present. Otherwise return a random one.
-            for (DbReference dbRef : identityRefs) {
-                if (dbRef.getDbAc().equals(CvDatabase.UNIPROT_MI_REF)) {
+            for ( DbReference dbRef : identityRefs ) {
+                if ( dbRef.getDbAc().equals( CvDatabase.UNIPROT_MI_REF ) ) {
 
                     return dbRef;
                 }
             }
 
-             return identityRefs.iterator().next();
+            return identityRefs.iterator().next();
         }
 
-        if (!dbRefs.isEmpty()) {
+        if ( !dbRefs.isEmpty() ) {
             return dbRefs.iterator().next();
         }
 
         return null;
     }
 
-    private static DbReference getPrimaryReference(Collection<DbReference> dbRefs) {
-        for (DbReference dbRef : dbRefs) {
-            if (dbRef.getRefTypeAc() != null && dbRef.getRefTypeAc().equals(CvXrefQualifier.PRIMARY_REFERENCE_MI_REF)) {
+    private static DbReference getPrimaryReference( Collection<DbReference> dbRefs ) {
+        for ( DbReference dbRef : dbRefs ) {
+            if ( dbRef.getRefTypeAc() != null && dbRef.getRefTypeAc().equals( CvXrefQualifier.PRIMARY_REFERENCE_MI_REF ) ) {
                 return dbRef;
             }
         }
 
-        if (!dbRefs.isEmpty()) {
+        if ( !dbRefs.isEmpty() ) {
             return dbRefs.iterator().next();
         }
 
         return null;
     }
-
 }
