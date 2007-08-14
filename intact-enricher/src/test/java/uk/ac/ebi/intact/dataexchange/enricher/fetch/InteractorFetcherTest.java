f81befabe0f6a7092f756fef60ef7cdfcee474fd
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-08-14 16:58:50 +0000

    Added test for InteractorFetcher

---
+++ intact-enricher/src/test/java/uk/ac/ebi/intact/dataexchange/enricher/fetch/InteractorFetcherTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2001-2007 The European Bioinformatics Institute.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.intact.dataexchange.enricher.fetch;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.ebi.intact.uniprot.model.UniprotProtein;
+
+/**
+ * TODO comment this
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ */
+public class InteractorFetcherTest {
+
+    private InteractorFetcher fetcher;
+
+    @Before
+    public void before() throws Exception {
+        fetcher = InteractorFetcher.getInstance();
+    }
+
+    @Test
+    public void fetchFromUniprot() throws Exception {
+        UniprotProtein uniprotProtein = fetcher.fetchInteractorFromUniprot("MK01_HUMAN", 9606);
+        Assert.assertNotNull(uniprotProtein);
+        Assert.assertEquals("P28482", uniprotProtein.getPrimaryAc());
+        
+        uniprotProtein = fetcher.fetchInteractorFromUniprot("MK01_HUMAN", 9606);
+        Assert.assertNotNull(uniprotProtein);
+        Assert.assertEquals("P28482", uniprotProtein.getPrimaryAc());
+    }
+}
