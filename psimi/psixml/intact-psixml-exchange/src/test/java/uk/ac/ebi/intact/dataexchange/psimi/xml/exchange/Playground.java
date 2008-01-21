eee046640301276a1193406b8bafb139683dac50
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2008-01-21 12:44:59 +0000

    More information given by the playground

--- psimi/psixml/intact-psixml-exchange/src/test/java/uk/ac/ebi/intact/dataexchange/psimi/xml/exchange/Playground.java
+++ psimi/psixml/intact-psixml-exchange/src/test/java/uk/ac/ebi/intact/dataexchange/psimi/xml/exchange/Playground.java
@@ -20,12 +20,14 @@ import uk.ac.ebi.intact.context.IntactEnvironment;
 import uk.ac.ebi.intact.dataexchange.enricher.EnricherConfig;
 import uk.ac.ebi.intact.dataexchange.enricher.EnricherContext;
 import uk.ac.ebi.intact.dataexchange.psimi.xml.exchange.enricher.PsiEnricher;
-import uk.ac.ebi.intact.model.Interaction;
-import uk.ac.ebi.intact.model.Component;
+import uk.ac.ebi.intact.model.*;
+import uk.ac.ebi.intact.core.persister.stats.PersisterStatistics;
 
 import java.io.*;
 import java.util.Properties;
 import java.util.Iterator;
+
+import org.joda.time.Interval;
 
 /**
  * TODO comment this
@@ -36,18 +38,43 @@ import java.util.Iterator;
 public class Playground {
 
     public static void main(String[] args) throws Exception {
-        //File fileToImport = new File("/ebi/sp/pro6/intact/local/data/curation-material/bushell/bushell-2008-1.xml");
-        File fileToImport = new File("/tmp/titz_trepa_interactions_psi_mi.xml");
+        File fileToImport = new File("/ebi/sp/pro6/intact/local/data/curation-material/titz-2008/titz-2008-1.xml");
+        //File fileToImport = new File("/tmp/titz_trepa_interactions_psi_mi.xml");
 
         Properties props = new Properties();
         props.put(IntactEnvironment.INSTITUTION_LABEL.getFqn(), "ebi");
         props.put(IntactEnvironment.AC_PREFIX_PARAM_NAME.getFqn(), "EBI");
 
-        File hibernateFile = new File(Playground.class.getResource("/playground-hibernate.cfg.xml").getFile());
+        File hibernateFile = new File(Playground.class.getResource("/d003-hibernate.cfg.xml").getFile());
 
         IntactContext.initStandaloneContext(hibernateFile);
         IntactContext.getCurrentInstance().getUserContext().setUserId("BARANDA");
 
+        long startTime = System.currentTimeMillis();
+
+        File tempFile = enrich(fileToImport);
+        //File tempFile = new File("/tmp", "psiToImport-9971.xml");  // titz
+
+        long timeAfterEnrich = System.currentTimeMillis();
+
+        importIntoIntact(tempFile);
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Imported file: "+tempFile);
+
+        Interval enrichingTime = new Interval(startTime, timeAfterEnrich);
+        Interval importTime = new Interval(timeAfterEnrich, endTime);
+        Interval totalTime = new Interval(startTime, endTime);
+
+        System.out.println("\nEnriching time: "+enrichingTime.toDurationMillis()+"ms");
+        System.out.println("Import time: "+importTime.toDurationMillis()+"ms");
+        System.out.println("Total time: "+totalTime.toDurationMillis()+"ms");
+
+
+    }
+
+    private static File enrich(File fileToImport) throws IOException {
         EnricherConfig enricherConfig = new EnricherConfig();
         enricherConfig.setUpdateInteractionShortLabels(true);
 
@@ -61,9 +88,10 @@ public class Playground {
         Writer writer = new FileWriter(tempFile);
 
         PsiEnricher.enrichPsiXml(is, writer, enricherConfig);
+        return tempFile;
+    }
 
-        writer.flush();
-
+    private static void importIntoIntact(File tempFile) throws FileNotFoundException {
         InputStream enricherInput = new FileInputStream(tempFile);
 
         int interactionsBefore = IntactContext.getCurrentInstance().getDataContext().getDaoFactory()
@@ -71,17 +99,27 @@ public class Playground {
         int experimentsBefore = IntactContext.getCurrentInstance().getDataContext().getDaoFactory()
                 .getExperimentDao().countAll();
 
-        PsiExchange.importIntoIntact(enricherInput);
+        PersisterStatistics stats = PsiExchange.importIntoIntact(enricherInput);
 
         int interactionsAfter = IntactContext.getCurrentInstance().getDataContext().getDaoFactory()
                 .getInteractionDao().countAll();
         int experimentsAfter = IntactContext.getCurrentInstance().getDataContext().getDaoFactory()
                 .getExperimentDao().countAll();
 
-        System.out.println("Interactions Created: " + (interactionsAfter-interactionsBefore));
-        System.out.println("Experiments Created: " + (experimentsAfter-experimentsBefore));
+        System.out.println("Real Interactions Created: " + (interactionsAfter-interactionsBefore));
+        System.out.println("Real Experiments Created: " + (experimentsAfter-experimentsBefore)+"\n");
 
+        System.out.println("Interactions Created: " + (stats.getPersistedCount(InteractionImpl.class, false)));
+        System.out.println("Experiments Created: " + (stats.getPersistedCount(Experiment.class, false)));
+        System.out.println("Proteins Created: " + (stats.getPersistedCount(ProteinImpl.class, false)));
+        System.out.println("Small Molecule Created: " + (stats.getPersistedCount(SmallMoleculeImpl.class, false)));
+        System.out.println("CvObjects Created: " + (stats.getPersistedCount(CvObject.class, true)));
+        System.out.println("BioSources Created: " + (stats.getPersistedCount(BioSource.class, false)));
+        System.out.println("Features Created: " + (stats.getPersistedCount(Feature.class, false)));
+        System.out.println("Components Created: " + (stats.getPersistedCount(Component.class, false)));
 
+        //  TODO: print duplicate interactions
+        System.out.println("\nDuplicated interactions: "+stats.getDuplicates(InteractionImpl.class, false));
     }
 
 
