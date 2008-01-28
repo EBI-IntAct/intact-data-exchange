672c875dd223742a73272807af20d4b4f7ebf468
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2008-01-28 21:18:40 +0000

    Created IntactTab2Xml

---
+++ psimi/psimitab/intact-psimitab/src/main/java/uk/ac/ebi/intact/psimitab/IntactTab2Xml.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2001-2008 The European Bioinformatics Institute.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.intact.psimitab;
+
+import psidev.psi.mi.tab.converter.xml2tab.Xml2Tab;
+import psidev.psi.mi.tab.converter.tab2xml.Tab2Xml;
+
+/**
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ */
+public class IntactTab2Xml extends Tab2Xml {
+
+    public IntactTab2Xml() {
+        final IntActColumnHandler columnHandler = new IntActColumnHandler();
+        setColumnHandler(columnHandler);
+        setBinaryInteractionClass( IntActBinaryInteraction.class );
+    }
+}
