da67227f1c5beb1ab97883082bf8c85bb043dab6
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2008-04-03 08:43:54 +0000

    Commenting out the unused cacheManagerEventListenerFactory in the ehcache file

--- intact-enricher/src/main/java/uk/ac/ebi/intact/dataexchange/enricher/EnricherContext.java
+++ intact-enricher/src/main/java/uk/ac/ebi/intact/dataexchange/enricher/EnricherContext.java
@@ -23,6 +23,7 @@ import uk.ac.ebi.intact.dataexchange.cvutils.PSILoader;
 import uk.ac.ebi.intact.dataexchange.cvutils.PsiLoaderException;
 import uk.ac.ebi.intact.dataexchange.cvutils.model.IntactOntology;
 
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -56,8 +57,8 @@ public class EnricherContext {
     private EnricherContext() {
         this.config = new EnricherConfig();
 
-        //InputStream ehcacheConfig = EnricherContext.class.getResourceAsStream("/META-INF/ehcache-enricher.xml");
-        //CacheManager.create(ehcacheConfig);
+        InputStream ehcacheConfig = EnricherContext.class.getResourceAsStream("/META-INF/ehcache-enricher.xml");
+        CacheManager.create(ehcacheConfig);
     }
 
     public EnricherConfig getConfig() {
