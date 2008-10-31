cb44cb24b71f2cd4e2995ed23c9bcdb00408cc80
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2008-10-31 14:16:53 +0000

    Remove field 'alias' as it is already contained in 'identifier'

--- psimi/psimitab/intact-psimitab/src/main/java/uk/ac/ebi/intact/psimitab/search/IntactSearchEngine.java
+++ psimi/psimitab/intact-psimitab/src/main/java/uk/ac/ebi/intact/psimitab/search/IntactSearchEngine.java
@@ -43,7 +43,6 @@ public class IntactSearchEngine extends AbstractSearchEngine<IntactBinaryInterac
         DocumentDefinition docDef = new IntactDocumentDefinition();
 
         return new String[]{"identifier",
-                            "alias",
                             docDef.getColumnDefinition(IntactDocumentDefinition.PUB_ID).getShortName(),
                             docDef.getColumnDefinition(IntactDocumentDefinition.PUB_AUTH).getShortName(),
                             "species",
