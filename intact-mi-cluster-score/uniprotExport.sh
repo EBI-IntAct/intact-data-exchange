b673955e94ac34bdac5290b9bb7b3b5205ac29e1
marine.dumousseau <marine.dumousseau@wanadoo.fr>
2011-03-31 15:56:21 +0000

    Fixed a bug in uniprotExport.sh

--- intact-mi-cluster-score/uniprotExport.sh
+++ intact-mi-cluster-score/uniprotExport.sh
@@ -5,4 +5,15 @@ export MAVEN_OPTS
 
 echo "MAVEN_OPTS=$MAVEN_OPTS"
 
-mvn clean install -Puniprot-export -Drule=$1 -Dsource=$2 -Ddrfile=$3 -Dccfile=$4 -Dgofile=$5 -Ddatabase=$6 -DbinaryOnly=$7 -DhighConfidence=$8 -DproteinOnly=$9 -DpositiveOnly=$10 -Dmaven.test.skip
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+echo $6
+echo $7
+echo $8
+echo $9
+echo $10
+
+mvn clean install -Puniprot-export -Drule=$1 -Dsource=$2 -Ddrfile=$3 -Dccfile=$4 -Dgofile=$5 -Ddatabase=$6 -DbinaryOnly=$7 -DhighConfidence=$8 -DproteinOnly=$9 -DpositiveOnly=${10} -Dmaven.test.skip
