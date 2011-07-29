0aa9f01943e3b60350a41fd8dba25071a6c265bf
mdumousseau <mdumousseau@yahoo.com>
2011-07-29 10:08:30 +0000

    updated the script so it uses settings and profiles to connect to the database

--- intact-mi-cluster-score/UniprotExportFilter.sh
+++ intact-mi-cluster-score/UniprotExportFilter.sh
@@ -7,4 +7,4 @@ echo "MAVEN_OPTS=$MAVEN_OPTS"
 
 MAVEN_PROFILE=$3
 
-mvn clean install -Puniprot-export-filter,${MAVEN_PROFILE} -Dsource=$1 -Dresults=$2 -DbinaryOnly=$4 -DhighConfidence=$5 -DproteinOnly=$6 -DpositiveOnly=$7 -Dmaven.test.skip
+mvn clean install -Puniprot-export-filter,${MAVEN_PROFILE} -Dsource=$1 -Dresults=$2 -DbinaryOnly=$4 -DhighConfidence=$5 -DproteinOnly=$6 -DpositiveOnly=$7 -Dmaven.test.skip -Ddb=oracle
