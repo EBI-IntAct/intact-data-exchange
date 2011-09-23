f574869b6f4e34397d91eb060f92f62b36b20997
mdumousseau <mdumousseau@yahoo.com>
2011-09-23 10:49:21 +0000

    Will have to add the tests in PsiXmlCreationTest

---
+++ intact-tasks/src/test/java/uk/ac/ebi/intact/task/PsiXmlCreationTest.java
@@ -0,0 +1,14 @@
+package uk.ac.ebi.intact.task;
+
+/**
+ * Unit tester for psi-xml generator
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>23/09/11</pre>
+ */
+
+public class PsiXmlCreationTest {
+
+
+}
