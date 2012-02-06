8fd4cbdcef885142dab73981362dec41be361747
mdumousseau <mdumousseau@yahoo.com>
2012-02-06 16:15:56 +0000

    BinaryPair : class implementing Comparable and which contains two string representing two interactor identifiers

---
+++ intact-tasks/src/main/java/uk/ac/ebi/intact/task/mitab/MitabClusterScoreItemProcessor.java
@@ -0,0 +1,176 @@
+package uk.ac.ebi.intact.task.mitab;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemStream;
+import org.springframework.batch.item.ItemStreamException;
+import psidev.psi.mi.tab.PsimiTabReader;
+import psidev.psi.mi.tab.model.BinaryInteraction;
+import psidev.psi.mi.tab.model.Confidence;
+import psidev.psi.mi.tab.model.ConfidenceImpl;
+import psidev.psi.mi.tab.model.Interactor;
+import psidev.psi.mi.xml.converter.ConverterException;
+import uk.ac.ebi.intact.task.mitab.clustering.BinaryPair;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * This processor retrieves cluster score for a binary interactions.
+ *
+ * It needs a MITAB file containing unique identifiers for both first and second interactors and a valid numerical score
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>06/02/12</pre>
+ */
+
+public class MitabClusterScoreItemProcessor implements ItemProcessor<BinaryInteraction, BinaryInteraction>, ItemStream {
+
+    private static final Log log = LogFactory.getLog(MitabClusterScoreItemProcessor.class);
+
+    private File clusteredMitabFile;
+    private Map<BinaryPair, Double> scores;
+    private String miScoreLabel;
+    private boolean hasHeader = false;
+
+    public MitabClusterScoreItemProcessor(){
+        scores = new TreeMap<BinaryPair, Double>();
+    }
+
+    @Override
+    public BinaryInteraction process(BinaryInteraction item) throws Exception {
+        
+        if (item == null){
+            return null; 
+        }
+        
+        Interactor interactorA = item.getInteractorA();
+        Interactor interactorB = item.getInteractorB();
+        
+        String firstInteractor = extractInteractorIdentifier(interactorA);
+        String secondInteractor = extractInteractorIdentifier(interactorB);
+        
+        BinaryPair currentPair = new BinaryPair(firstInteractor, secondInteractor);
+        
+        if (scores.containsKey(currentPair)){
+            Double score = scores.get(currentPair);
+
+            Confidence conf = new ConfidenceImpl(miScoreLabel, Double.toString(score));
+            item.getConfidenceValues().add(conf);
+        }
+        
+        return item;
+    }
+
+    public File getClusteredMitabFile() {
+        return clusteredMitabFile;
+    }
+
+    public void setClusteredMitabFile(File clusteredMitabFile) {
+        this.clusteredMitabFile = clusteredMitabFile;
+    }
+
+    public String getMiScoreLabel() {
+        return miScoreLabel;
+    }
+
+    public void setMiScoreLabel(String miScoreLabel) {
+        this.miScoreLabel = miScoreLabel;
+    }
+
+    /**
+     *
+     * @param interactor
+     * @return the first identifier of an interactor, null otherwise
+     */
+    private String extractInteractorIdentifier(Interactor interactor){
+
+        if (interactor == null){
+            return null;
+        }
+
+        if (!interactor.getIdentifiers().isEmpty()){
+            if (interactor.getIdentifiers().size() > 1){
+                log.warn("Interactor with more than one identifiers : " + interactor.toString() + ". Only the first identifier is taken into account");
+            }
+
+            return interactor.getIdentifiers().iterator().next().getIdentifier();
+        }
+        else {
+            log.warn("Interactor without identifiers : " + interactor.toString() + ". This interactor will be ignored");
+        }
+
+        return null;
+    }
+
+    private Double extractClusterScoreFrom(List<Confidence> confidences){
+
+        for (Confidence conf : confidences){
+            if (conf.getType() != null && conf.getType().equalsIgnoreCase(miScoreLabel)){
+                try {
+                    Double score = Double.valueOf(conf.getValue());
+
+                    return score;
+                }
+                catch (NumberFormatException e){
+                    log.error("The mi score " + conf.getValue() + " is not a valid mi score and will be ignored.");
+                }
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public void open(ExecutionContext executionContext) throws ItemStreamException {
+
+        if (clusteredMitabFile == null){
+            throw new ItemStreamException("The Mitab cluster score item processor needs a clustered mitab file containing the scores to be able to process the non clustered MITAB file");
+        }
+
+        PsimiTabReader mitabReader = new PsimiTabReader(hasHeader);
+
+        try {
+            Iterator<BinaryInteraction> binaryInteractionIterator = mitabReader.iterate(clusteredMitabFile);
+
+            while (binaryInteractionIterator.hasNext()){
+                BinaryInteraction binary = binaryInteractionIterator.next();
+
+                Interactor interactorA = binary.getInteractorA();
+                Interactor interactorB = binary.getInteractorB();
+                List<Confidence> confidences = binary.getConfidenceValues();
+
+                String firstInteractor = extractInteractorIdentifier(interactorA);
+                String secondInteractor = extractInteractorIdentifier(interactorB);
+                Double score = extractClusterScoreFrom(confidences);
+
+                if (score != null){
+                    scores.put(new BinaryPair(firstInteractor, secondInteractor), score);
+                }
+            }
+
+        } catch (IOException e) {
+            throw new ItemStreamException("Impossible to read the clustered mitab file " + clusteredMitabFile.getAbsolutePath(), e);
+        } catch (ConverterException e) {
+            throw new ItemStreamException("Conversion problem while reading clustered mitab file " + clusteredMitabFile.getAbsolutePath(), e);
+        }
+    }
+
+    @Override
+    public void update(ExecutionContext executionContext) throws ItemStreamException {
+        // Nothing to update
+    }
+
+    @Override
+    public void close() throws ItemStreamException {
+
+        scores.clear();
+    }
+}
---
+++ intact-tasks/src/main/java/uk/ac/ebi/intact/task/mitab/clustering/BinaryPair.java
@@ -0,0 +1,212 @@
+package uk.ac.ebi.intact.task.mitab.clustering;
+
+/**
+ * A binary pair contains interactor A and B. Two binary pairs are equal if A and B are equals. A and B are interchangeable 
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>06/02/12</pre>
+ */
+
+public class BinaryPair implements Comparable<BinaryPair>{
+
+    private String interactorA;
+    private String interactorB;
+    
+    public BinaryPair(String interactorA, String interactorB){
+        this.interactorA = interactorA;
+        this.interactorB = interactorB;
+    }
+
+    public String getInteractorA() {
+        return interactorA;
+    }
+
+    public String getInteractorB() {
+        return interactorB;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        
+        if (interactorA == null && interactorB == null){
+            result = 0;
+        }
+        else if (interactorA == null){
+            result = interactorB.hashCode();
+        }
+        else if (interactorB == null){
+            result = interactorA.hashCode();
+        }
+        else {
+            int compare = interactorA.compareTo(interactorB);
+
+            if (compare == 0){
+                result = interactorA.hashCode();
+                result = 31 * interactorB.hashCode();
+            }
+            // interactor A is before interactor B
+            else if (compare < 0){
+                result = interactorA.hashCode();
+                result = 31 * interactorB.hashCode();
+            }
+            // interactor B is before interactor A
+            else {
+                result = interactorB.hashCode();
+                result = 31 * interactorA.hashCode(); 
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+
+        BinaryPair pair2 = (BinaryPair) o;
+
+        if (interactorA != null && interactorB != null){
+            if (pair2.getInteractorA() == null || pair2.getInteractorB() == null){
+                return false;
+            }
+            else if (interactorA.equals(pair2.getInteractorA())){
+                return interactorB.equals(pair2.getInteractorB());
+            }
+            else if (interactorA.equals(pair2.getInteractorB())){
+                return interactorB.equals(pair2.getInteractorA());
+            }
+            else {
+                return false;
+            }
+        }
+        else if (interactorA == null && interactorB == null){
+             if (pair2.getInteractorA() != null || pair2.getInteractorB() != null){
+                 return false;
+             }
+        }
+        else if (interactorA == null && interactorB != null){
+            if (pair2.getInteractorA() == null){
+                if (pair2.getInteractorB() == null){
+                    return false;
+                }
+                else {
+                    return pair2.getInteractorB().equals(interactorB);
+                }
+            }
+            else if (pair2.getInteractorB() == null){
+                if (pair2.getInteractorA() == null){
+                    return false;
+                }
+                else {
+                    return pair2.getInteractorA().equals(interactorB);
+                }
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            if (pair2.getInteractorA() == null){
+                if (pair2.getInteractorB() == null){
+                    return false;
+                }
+                else {
+                    return pair2.getInteractorB().equals(interactorA);
+                }
+            }
+            else if (pair2.getInteractorB() == null){
+                if (pair2.getInteractorA() == null){
+                    return false;
+                }
+                else {
+                    return pair2.getInteractorA().equals(interactorA);
+                }
+            }
+            else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    private String[] orderInteractors(String inter1, String inter2){
+        
+        if (inter1 == null && inter2 == null){
+            return new String[] {inter1, inter2};
+        }
+        else if (inter1 == null){
+            return new String[] {inter2, inter1};
+        }
+        else if (inter2 == null){
+            return new String[] {inter1, inter2};
+        }
+        else {
+            int comp = inter1.compareTo(inter2);
+            
+            if (comp == 0){
+                return new String[] {inter1, inter2};
+            }
+            else if (comp < 0){
+                return new String[] {inter1, inter2};
+            }
+            else {
+                return new String[] {inter2, inter1};
+            }
+        }
+    }
+
+    @Override
+    public int compareTo(BinaryPair binaryPair) {
+        final int BEFORE = -1;
+        final int AFTER = 1;
+        
+        String [] interactors1 = orderInteractors(interactorA, interactorB);
+        String [] interactors2 = orderInteractors(binaryPair.getInteractorA(), binaryPair.getInteractorB());
+        
+        String firstInteractor1 = interactors1[0];
+        String firstInteractor2 = interactors1[1];
+        String secondInteractor1 = interactors2[0];
+        String secondInteractor2 = interactors2[1];
+        
+        int comp1 = compareInteractors(firstInteractor1, firstInteractor2);
+
+        if (comp1 == 0){
+            return compareInteractors(secondInteractor1, secondInteractor2);
+        }
+        else if (comp1 < 0){
+            return BEFORE;
+        }
+        else {
+            return AFTER;
+        }
+    }
+
+    private int compareInteractors(String inter1, String inter2) {
+        final int EQUAL = 0;
+        final int BEFORE = -1;
+        final int AFTER = 1;
+        
+        if (inter1 == null && inter2 == null){
+            return EQUAL;
+        }
+        else if (inter1 == null){
+            return AFTER;
+        }
+        else if (inter2 == null){
+            return BEFORE;
+        }
+        else {
+            return inter1.compareTo(inter2);
+        }
+    }
+}
