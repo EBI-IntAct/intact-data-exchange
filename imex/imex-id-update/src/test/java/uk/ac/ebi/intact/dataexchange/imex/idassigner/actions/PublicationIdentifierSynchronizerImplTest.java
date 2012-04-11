b3a6e093145e9207e63a7765517a148b2f622b07
mdumousseau <mdumousseau@yahoo.com>
2012-04-11 14:41:23 +0000

    Added unit tests for synchronizing publication identifiers

--- imex/imex-id-update/src/test/java/uk/ac/ebi/intact/dataexchange/imex/idassigner/actions/PublicationIdentifierSynchronizerImplTest.java
+++ imex/imex-id-update/src/test/java/uk/ac/ebi/intact/dataexchange/imex/idassigner/actions/PublicationIdentifierSynchronizerImplTest.java
@@ -28,7 +28,7 @@ public class PublicationIdentifierSynchronizerImplTest extends IntactBasicTestCa
     @Autowired
     private PublicationIdentifierSynchronizer identifierSynchronizerTest;
     private Publication intactPub12345;
-    private Publication intactPub12346;
+    private Publication intactDoi;
     private Publication intactPub12347;
     private Publication intactPubUnassigned;
 
@@ -42,13 +42,13 @@ public class PublicationIdentifierSynchronizerImplTest extends IntactBasicTestCa
         intactPub12345.setImexAccession("IM-1");
         identifierSynchronizerTest.getImexCentralClient().createPublication(intactPub12345);
 
-        intactPub12346 = new Publication();
+        intactDoi = new Publication();
         Identifier pubmed2 = new Identifier();
-        pubmed2.setNs("pmid");
-        pubmed2.setAc("12346");
-        intactPub12346.getIdentifier().add(pubmed2);
-        intactPub12346.setImexAccession("IM-2");
-        identifierSynchronizerTest.getImexCentralClient().createPublication(intactPub12346);
+        pubmed2.setNs("doi");
+        pubmed2.setAc("123/1(a2)");
+        intactDoi.getIdentifier().add(pubmed2);
+        intactDoi.setImexAccession("IM-2");
+        identifierSynchronizerTest.getImexCentralClient().createPublication(intactDoi);
 
         intactPub12347 = new Publication();
         Identifier pubmed3 = new Identifier();
@@ -128,5 +128,65 @@ public class PublicationIdentifierSynchronizerImplTest extends IntactBasicTestCa
 
         Assert.assertEquals("unassigned604", intactPubUnassigned.getIdentifier().iterator().next().getAc());
         intactPubUnassigned.getIdentifier().clear();
+    }
+
+    @Test
+    @Transactional(propagation = Propagation.NEVER)
+    @DirtiesContext
+    public void synchronized_mismatch_pubmedId_aborted() throws ImexCentralException {
+
+        uk.ac.ebi.intact.model.Publication intactPublication = new uk.ac.ebi.intact.model.Publication(getIntactContext().getInstitution(), "12346");
+
+        try {
+            identifierSynchronizerTest.synchronizePublicationIdentifier(intactPublication, intactPub12345);
+            Assert.assertFalse(true);
+        } catch (PublicationImexUpdaterException e) {
+            Assert.assertEquals("12345", intactPub12345.getIdentifier().iterator().next().getAc());
+        }
+    }
+
+    @Test
+    @Transactional(propagation = Propagation.NEVER)
+    @DirtiesContext
+    public void synchronized_mismatch_unassigned_intact_aborted() throws ImexCentralException {
+
+        uk.ac.ebi.intact.model.Publication intactPublication = new uk.ac.ebi.intact.model.Publication(getIntactContext().getInstitution(), "unassigned604");
+
+        try {
+            identifierSynchronizerTest.synchronizePublicationIdentifier(intactPublication, intactPub12345);
+            Assert.assertFalse(true);
+        } catch (PublicationImexUpdaterException e) {
+            Assert.assertEquals("12345", intactPub12345.getIdentifier().iterator().next().getAc());
+        }
+    }
+
+    @Test
+    @Transactional(propagation = Propagation.NEVER)
+    @DirtiesContext
+    public void synchronized_mismatch_doi_aborted() throws ImexCentralException {
+
+        uk.ac.ebi.intact.model.Publication intactPublication = new uk.ac.ebi.intact.model.Publication(getIntactContext().getInstitution(), "1234-5(7a)");
+
+        try {
+            identifierSynchronizerTest.synchronizePublicationIdentifier(intactPublication, intactDoi);
+            Assert.assertFalse(true);
+        } catch (PublicationImexUpdaterException e) {
+            Assert.assertEquals("123/1(a2)", intactDoi.getIdentifier().iterator().next().getAc());
+        }
+    }
+
+    @Test
+    @Transactional(propagation = Propagation.NEVER)
+    @DirtiesContext
+    public void synchronized_new_identifier_already_existing_aborted() throws PublicationImexUpdaterException {
+
+        uk.ac.ebi.intact.model.Publication intactPublication = new uk.ac.ebi.intact.model.Publication(getIntactContext().getInstitution(), "12345");
+
+        try {
+            identifierSynchronizerTest.synchronizePublicationIdentifier(intactPublication, intactPubUnassigned);
+            Assert.assertFalse(true);
+        } catch (ImexCentralException e) {
+            Assert.assertTrue(intactPubUnassigned.getIdentifier().isEmpty());
+        }
     }
 }
