4c741a3ae51924b770e505f173c6431e0af56fb0
mdumousseau <mdumousseau@yahoo.com>
2012-04-12 12:46:57 +0000

    GlobalImexPublicationUpdater : reuse existing public static variables from DefaultImexCentralClient

--- imex/imex-id-update/src/main/java/uk/ac/ebi/intact/dataexchange/imex/idassigner/GlobalImexPublicationUpdater.java
+++ imex/imex-id-update/src/main/java/uk/ac/ebi/intact/dataexchange/imex/idassigner/GlobalImexPublicationUpdater.java
@@ -3,6 +3,7 @@ package uk.ac.ebi.intact.dataexchange.imex.idassigner;
 import edu.ucla.mbi.imex.central.ws.v20.IcentralFault;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import uk.ac.ebi.intact.bridges.imexcentral.DefaultImexCentralClient;
 import uk.ac.ebi.intact.bridges.imexcentral.ImexCentralException;
 import uk.ac.ebi.intact.dataexchange.imex.idassigner.actions.IntactPublicationCollector;
 import uk.ac.ebi.intact.dataexchange.imex.idassigner.actions.PublicationImexUpdaterException;
@@ -25,18 +26,6 @@ public class GlobalImexPublicationUpdater {
     private static final Log log = LogFactory.getLog(GlobalImexPublicationUpdater.class);
 
     private IntactPublicationCollector intactPublicationCollector;
-    public static int USER_NOT_AUTHORIZED = 2;
-    public static int OPERATION_NOT_VALID = 3;
-    public static int IDENTIFIER_MISSING = 4;
-    public static int IDENTIFIER_UNKNOWN = 5;
-    public static int NO_RECORD = 6;
-    public static int NO_RECORD_CREATED = 7;
-    public static int STATUS_UNKNOWN = 8;
-    public static int NO_IMEX_ID = 9;
-    public static int UNKNOWN_USER = 10;
-    public static int UNKNOWN_GROUP = 11;
-    public static int OPERATION_NOT_SUPPORTED = 98;
-    public static int INTERNAL_SERVER_ERROR = 99;
 
     public ImexCentralManager getImexCentralManager() {
         return imexCentralManager;
@@ -92,51 +81,51 @@ public class GlobalImexPublicationUpdater {
     }
 
     private void processImexCentralException(String publication, ImexCentralException e, IcentralFault f) {
-        if( f.getFaultInfo().getFaultCode() == USER_NOT_AUTHORIZED ) {
+        if( f.getFaultInfo().getFaultCode() == DefaultImexCentralClient.USER_NOT_AUTHORIZED ) {
             ImexErrorEvent errorEvt = new ImexErrorEvent(this, ImexErrorType.user_not_authorized, publication, null, null, null, "missing/unknown user and/or missing/invalid password provided : " + e.getMessage());
             imexCentralManager.fireOnImexError(errorEvt);
         }
-        else if( f.getFaultInfo().getFaultCode() == OPERATION_NOT_VALID ) {
+        else if( f.getFaultInfo().getFaultCode() == DefaultImexCentralClient.OPERATION_NOT_VALID ) {
             ImexErrorEvent errorEvt = new ImexErrorEvent(this, ImexErrorType.operation_not_valid, publication, null, null, null, "invalid operation : " + e.getMessage());
             imexCentralManager.fireOnImexError(errorEvt);
         }
-        else if( f.getFaultInfo().getFaultCode() == IDENTIFIER_MISSING ) {
+        else if( f.getFaultInfo().getFaultCode() == DefaultImexCentralClient.IDENTIFIER_MISSING ) {
             ImexErrorEvent errorEvt = new ImexErrorEvent(this, ImexErrorType.identifier_missing, publication, null, null, null, "missing identifier : " + e.getMessage());
             imexCentralManager.fireOnImexError(errorEvt);
         }
-        else if( f.getFaultInfo().getFaultCode() == IDENTIFIER_UNKNOWN ) {
+        else if( f.getFaultInfo().getFaultCode() == DefaultImexCentralClient.IDENTIFIER_UNKNOWN ) {
             ImexErrorEvent errorEvt = new ImexErrorEvent(this, ImexErrorType.identifier_unknown, publication, null, null, null, "unrecognized identifier : " + e.getMessage());
             imexCentralManager.fireOnImexError(errorEvt);
         }
-        else if( f.getFaultInfo().getFaultCode() == NO_RECORD ) {
+        else if( f.getFaultInfo().getFaultCode() == DefaultImexCentralClient.NO_RECORD ) {
             ImexErrorEvent errorEvt = new ImexErrorEvent(this, ImexErrorType.no_record, publication, null, null, null, "query returned no records : " + e.getMessage());
             imexCentralManager.fireOnImexError(errorEvt);
         }
-        else if( f.getFaultInfo().getFaultCode() == NO_RECORD_CREATED ) {
+        else if( f.getFaultInfo().getFaultCode() == DefaultImexCentralClient.NO_RECORD_CREATED ) {
             ImexErrorEvent errorEvt = new ImexErrorEvent(this, ImexErrorType.no_record_created, publication, null, null, null, "requested records were not created : " + e.getMessage());
             imexCentralManager.fireOnImexError(errorEvt);
         }
-        else if( f.getFaultInfo().getFaultCode() == STATUS_UNKNOWN ) {
+        else if( f.getFaultInfo().getFaultCode() == DefaultImexCentralClient.STATUS_UNKNOWN ) {
             ImexErrorEvent errorEvt = new ImexErrorEvent(this, ImexErrorType.status_unknown, publication, null, null, null, "unknown status : " + e.getMessage());
             imexCentralManager.fireOnImexError(errorEvt);
         }
-        else if( f.getFaultInfo().getFaultCode() == NO_IMEX_ID ) {
+        else if( f.getFaultInfo().getFaultCode() == DefaultImexCentralClient.NO_IMEX_ID ) {
             ImexErrorEvent errorEvt = new ImexErrorEvent(this, ImexErrorType.no_IMEX_id, publication, null, null, null, "IMEx identifier missing : " + e.getMessage());
             imexCentralManager.fireOnImexError(errorEvt);
         }
-        else if( f.getFaultInfo().getFaultCode() == UNKNOWN_USER ) {
+        else if( f.getFaultInfo().getFaultCode() == DefaultImexCentralClient.UNKNOWN_USER ) {
             ImexErrorEvent errorEvt = new ImexErrorEvent(this, ImexErrorType.unknown_user, publication, null, null, null, "passed user parameter does not specify a known user : " + e.getMessage());
             imexCentralManager.fireOnImexError(errorEvt);
         }
-        else if( f.getFaultInfo().getFaultCode() == UNKNOWN_GROUP ) {
+        else if( f.getFaultInfo().getFaultCode() == DefaultImexCentralClient.UNKNOWN_GROUP ) {
             ImexErrorEvent errorEvt = new ImexErrorEvent(this, ImexErrorType.unknown_group, publication, null, null, null, "passed group parameter does not specify a known group : " + e.getMessage());
             imexCentralManager.fireOnImexError(errorEvt);
         }
-        else if( f.getFaultInfo().getFaultCode() == OPERATION_NOT_SUPPORTED ) {
+        else if( f.getFaultInfo().getFaultCode() == DefaultImexCentralClient.OPERATION_NOT_SUPPORTED ) {
             ImexErrorEvent errorEvt = new ImexErrorEvent(this, ImexErrorType.operation_not_supported, publication, null, null, null, "operation not (yet) supported : " + e.getMessage());
             imexCentralManager.fireOnImexError(errorEvt);
         }
-        else if( f.getFaultInfo().getFaultCode() == INTERNAL_SERVER_ERROR ) {
+        else if( f.getFaultInfo().getFaultCode() == DefaultImexCentralClient.INTERNAL_SERVER_ERROR ) {
             ImexErrorEvent errorEvt = new ImexErrorEvent(this, ImexErrorType.internal_server_error, publication, null, null, null, "internal server error : " + e.getMessage());
             imexCentralManager.fireOnImexError(errorEvt);
         }
