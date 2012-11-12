084ba5308d5aac23ff5fd154697d8de9326524b5
mdumousseau <mdumousseau@yahoo.com>
2012-11-12 08:49:20 +0000

    [maven-scm] copy for tag intact-tasks-2.1.6.4

--- intact-mi-cluster-score/exportFilter.sh
+++
@@ -1,8 +0,0 @@
-MAVEN_OPTS="-Xms512m -Xmx2024m -XX:MaxPermSize=256m"
-
-export MAVEN_OPTS
-
-
-echo "MAVEN_OPTS=$MAVEN_OPTS"
-
-mvn clean install -Pexport-filter -DfileA=$1 -DfileB=$2 -Dresults=$3 -Dmaven.test.skip
