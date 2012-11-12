6bb57b7df29001672afba17270148f9fd2a44c8b
mdumousseau <mdumousseau@yahoo.com>
2012-11-12 11:10:21 +0000

    [maven-scm] copy for tag imex-id-update-2.1.6.1

--- uniprot-export/intact-uniprot-export/src/main/java/uk/ac/ebi/intact/util/uniprotExport/miscore/extension/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * The package contains the classes extending the mi cluster project for graphical representation of the mi score
- *
- */
-package uk.ac.ebi.intact.util.uniprotExport.miscore.extension;
