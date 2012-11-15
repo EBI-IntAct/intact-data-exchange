34b920414b63ec2b2d2359699fd7c56d74d07cef
mdumousseau <mdumousseau@yahoo.com>
2012-11-15 16:42:54 +0000

    [maven-scm] copy for tag intact-solr-home-2.1.6.1

--- uniprot-export/intact-uniprot-export/src/main/java/uk/ac/ebi/intact/util/uniprotExport/converters/encoreconverters/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * The package contains the classes responsible for converting an Encore interaction
- * into a CC line or GO line.
- *
- */
-package uk.ac.ebi.intact.util.uniprotExport.converters.encoreconverters;
