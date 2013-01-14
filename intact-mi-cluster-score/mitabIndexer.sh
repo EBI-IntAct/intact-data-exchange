dfe7b65eb3c4d4cac7963f0766ae6a4a729dca0d
mdumousseau <mdumousseau@yahoo.com>
2013-01-14 10:19:08 +0000

    [maven-scm] copy for tag intact-uniprot-export-2.1.6.5

--- intact-mi-cluster-score/mitabIndexer.sh
+++
@@ -1,8 +0,0 @@
-MAVEN_OPTS="-Xms512m -Xmx2024m -XX:MaxPermSize=256m"
-
-export MAVEN_OPTS
-
-
-echo "MAVEN_OPTS=$MAVEN_OPTS"
-
-mvn -U clean install -Pmitab-indexer -Dmitab=$1 -Dclustered=$2 -Dmaven.test.skip
