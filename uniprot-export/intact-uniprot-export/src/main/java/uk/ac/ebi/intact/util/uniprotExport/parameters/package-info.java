75e9dae742cd05589e3bea2441e359cfe1c6eff4
mdumousseau <mdumousseau@yahoo.com>
2013-07-17 15:25:17 +0000

    [maven-scm] copy for tag structured-abstract-2.1.8-alpha-2

--- uniprot-export/intact-uniprot-export/src/main/java/uk/ac/ebi/intact/util/uniprotExport/parameters/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * The package contains the classes representing the parameters for the uniprot export writers (parameters for DR line, CC line and GO line)
- *
- */
-package uk.ac.ebi.intact.util.uniprotExport.parameters;
