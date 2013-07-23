e55dc856c425f708fde74e34eb85a88b73041d80
oscar.forner.martinez <oscar.forner.martinez@gmail.com>
2013-07-23 16:05:11 +0000

    I began with the ComplexIndexer.java

--- psimi/psimitab/intact-solr/src/main/java/uk/ac/ebi/intact/dataexchange/psimi/solr/complex/ComplexIndexer.java
+++ psimi/psimitab/intact-solr/src/main/java/uk/ac/ebi/intact/dataexchange/psimi/solr/complex/ComplexIndexer.java
@@ -1,10 +1,29 @@
 package uk.ac.ebi.intact.dataexchange.psimi.solr.complex;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import uk.ac.ebi.intact.dataexchange.psimi.solr.SolrLogger;
+
 /**
  * @author Oscar Forner (oforner@ebi.ac.uk)
  * @version $Id$
  * @since 23/07/13
  */
 public class ComplexIndexer {
+
+    private static final Log log = LogFactory.getLog( ComplexIndexer.class );
+
+    private HttpSolrServer solrServer;
+
+    private int commitInterval = 50000;
+    private int numberOfTries = 5;
+
+    public ComplexIndexer(HttpSolrServer solrServer){
+        // Receive a HttpSolrServer such as parameter
+        this.solrServer = solrServer;
+        // Activate the Log
+        SolrLogger.readFromLog4j();
+    }
 
 }
