38763f97a21ecb98481f81a735423c3094d0843e
mdumousseau <mdumousseau@yahoo.com>
2014-11-18 09:58:53 +0000

    Updated importer

---
+++ intact-db-importer/import_complex.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# == 3 ];
+then
+      INPUT_FILE=$1;
+      USER_ID=$2;
+      PROFILE=$3;
+      echo "Input file: ${INPUT_FILE}"
+      echo "User: ${USER_ID}"
+      echo "Profile: ${PROFILE}"
+      mvn -U clean install -Pimport,${PROFILE} -Dmi.file=${INPUT_FILE} -Djami.user.context.id=${USER_ID} -Djob.name=complexImport -Dmaven.test.skip
+else
+      echo ""
+      echo "ERROR: wrong number of parameters ($#)."
+      echo "usage: INPUT_FILE USER_ID PROFILE"
+      echo "usage: INPUT_FILE: the name of the file to enrich."
+      echo "usage: USER_ID: the name of the user importing data"
+      echo "usage: PROFILE: the name of the profile"
+      echo ""
+      exit 1
+fi
---
+++ intact-db-importer/restart_import_evidence.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# == 4 ];
+then
+      INPUT_FILE=$1;
+      USER_ID=$2;
+      JOBID=$3;
+      PROFILE=$4;
+      echo "Input file: ${INPUT_FILE}"
+      echo "User: ${USER_ID}"
+      echo "Job id: ${JOBID}"
+      echo "Profile: ${PROFILE}"
+      mvn -U install -restart-import,${PROFILE} -Dmi.file=${INPUT_FILE} -Djami.user.context.id=${USER_ID} -Djob.name=interactionEvidenceImport -Djob.id=${JOBID} -Dmaven.test.skip
+else
+      echo ""
+      echo "ERROR: wrong number of parameters ($#)."
+      echo "usage: INPUT_FILE USER_ID PROFILE"
+      echo "usage: INPUT_FILE: the name of the file to enrich."
+      echo "usage: USER_ID: the name of the user importing data"
+      echo "usage: JOBID: the id of the job retrieved from the logs"
+      echo "usage: PROFILE: the name of the profile"
+      echo ""
+      exit 1
+fi
