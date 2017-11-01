2db91313d33006c914aa11bce9c264f83764de41
maximiliankoch <tschaka1904@gmail.com>
2017-11-01 22:03:33 +0100

    Clean up and use pull to kill thread if we can't find anything.

--- intact-mutation-export/src/main/java/uk/ac/ebi/intact/export/mutation/helper/Converter.java
+++ intact-mutation-export/src/main/java/uk/ac/ebi/intact/export/mutation/helper/Converter.java
@@ -8,33 +8,53 @@ import uk.ac.ebi.intact.export.mutation.helper.model.MutationExportLine;
 import uk.ac.ebi.intact.export.mutation.processor.MutationExportProcessor;
 import uk.ac.ebi.intact.jami.model.extension.IntactFeatureEvidence;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by Maximilian Koch (mkoch@ebi.ac.uk).
  */
-public class Consumer implements Runnable {
-    private static final Log log = LogFactory.getLog(Consumer.class);
-
+public class Converter implements Runnable {
+    private static final Log log = LogFactory.getLog(Converter.class);
 
     @Override
     @Transactional(propagation = Propagation.REQUIRED, value = "jamiTransactionManager", readOnly = true)
     public void run() {
         while (true) {
             IntactFeatureEvidence intactFeatureEvidence = null;
+            if (MutationExportProcessor.loadedAll && MutationExportProcessor.readyToConvertQueue.isEmpty()
+                    && MutationExportProcessor.readyToCheckQueue.isEmpty()) {
+                break;
+            }
             try {
-                intactFeatureEvidence = MutationExportProcessor.checkedMutations.take();
+                intactFeatureEvidence = MutationExportProcessor.readyToConvertQueue.poll(30, TimeUnit.SECONDS);
+                if(intactFeatureEvidence == null){
+                    continue;
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             FeatureToExportLine featureToExportLine = new FeatureToExportLine();
             log.info("Convert " + intactFeatureEvidence.getAc());
-            MutationExportLine line = featureToExportLine.convertFeatureToMutationExportLine(intactFeatureEvidence);
+            MutationExportLine line = null;
+            try {
+                line = featureToExportLine.convertFeatureToMutationExportLine(intactFeatureEvidence);
+
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+
+            }
             log.info("Converted " + intactFeatureEvidence);
             try {
-                MutationExportProcessor.exportMutations.put(line);
+                MutationExportProcessor.readyToExportQueue.put(line);
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+                System.out.println(intactFeatureEvidence.getAc());
             }
         }
+        log.info("Thread killed");
+        Thread.currentThread().interrupt();
     }
 }
 
