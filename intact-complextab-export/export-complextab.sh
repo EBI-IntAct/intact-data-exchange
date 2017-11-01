967ed8594fd717279d660a4be60d9c70b5b04a49
maximiliankoch <tschaka1904@gmail.com>
2017-11-01 22:12:48 +0100

    Clean up and use proper attr

--- intact-complextab-export/export-complextab.sh
+++ intact-complextab-export/export-complextab.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# Runs the protein update
+# Runs the complex export
 #
 # Usage $0 database folder
 #
 #
 ##################################################################
 
-if [ $# -ne 1 ]; then
+if [ $# -ne 3 ]; then
       echo ""
       echo "ERROR: wrong number of parameters ($#)."
       echo "usage: $0 DATABASE_PROFILE[ebi-test, ebi-prod, etc] FOLDER LOG_FILE"
@@ -15,9 +15,11 @@ if [ $# -ne 1 ]; then
       exit 1
 fi
 
+DATABASE=$1
 FOLDER=$2
+LOG=$3
 
 rm -rf target
 mkdir target
 
-mvn clean install -Pcomplextab-export,ebi-release,oracle -Dfolder=${FOLDER} -Dmaven.test.skip
+mvn clean install -Pcomplextab-export,${DATABASE},oracle -Dfolder=${FOLDER} -Dmaven.test.skip
