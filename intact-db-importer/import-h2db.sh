277d81770517737aa11c8902ba3979e8357a4c02
ntoro <ntoro@ebi.ac.uk>
2018-08-07 14:55:15 +0100

    GENISSUES-217: Creates a configuration for testing that allow to run the importer in a H2 file database and compare the result after the execution.

---
+++ intact-db-importer/import-h2db.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# == 3 ];
+then
+      INPUT_FILE=$1;
+      USER_ID=$2;
+      PROFILE=$3;
+      echo "Input file: ${INPUT_FILE}"
+      echo "User: ${USER_ID}"
+      echo "Profile: ${PROFILE}"
+      mvn -U clean install -import-h2,${PROFILE} -Dmi.file=${INPUT_FILE} -Djob.name=interactionMixImport -Dmaven.repo.local=repository -Derror.file=import_errors.log -Dmaven.test.skip
+else
+      echo ""
+      echo "ERROR: wrong number of parameters ($#)."
+      echo "usage: INPUT_FILE USER_ID PROFILE"
+      echo "usage: INPUT_FILE: the name of the file to enrich."
+      echo "usage: USER_ID: the name of the user importing data"
+      echo "usage: PROFILE: the name of the profile"
+      echo ""
+      exit 1
+fi
