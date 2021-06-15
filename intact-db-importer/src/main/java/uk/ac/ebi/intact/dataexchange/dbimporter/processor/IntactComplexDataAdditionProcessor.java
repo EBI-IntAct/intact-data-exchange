a7eb1a33cc72284d00269a1cc2f92131e9029a56
anjali <anjali@ebi.ac.uk>
2021-06-15 14:08:13 +0100

    GENISSUES-409: Complex importer: Added job id as complex annotation

--- intact-db-importer/src/main/java/uk/ac/ebi/intact/dataexchange/dbimporter/processor/IntactComplexDataAdditionProcessor.java
+++ intact-db-importer/src/main/java/uk/ac/ebi/intact/dataexchange/dbimporter/processor/IntactComplexDataAdditionProcessor.java
@@ -1,5 +1,8 @@
 package uk.ac.ebi.intact.dataexchange.dbimporter.processor;
 
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemStream;
@@ -7,7 +10,9 @@ import org.springframework.batch.item.ItemStreamException;
 import org.springframework.core.io.Resource;
 import org.springframework.util.Assert;
 import psidev.psi.mi.jami.model.Complex;
+import uk.ac.ebi.intact.jami.model.extension.InteractorAnnotation;
 import uk.ac.ebi.intact.jami.service.ComplexService;
+import uk.ac.ebi.intact.jami.utils.IntactUtils;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -22,6 +27,7 @@ public class IntactComplexDataAdditionProcessor implements ItemProcessor<Complex
     private Resource errorResource;
     private Writer errorWriter;
     private ComplexService complexService;
+    private String importTag;
 
     public Complex process(Complex item) throws Exception {
         if (this.complexService == null) {
@@ -38,6 +44,13 @@ public class IntactComplexDataAdditionProcessor implements ItemProcessor<Complex
             errorWriter.write("Cannot add complex ac xref  ");
         }
 
+        // add job id annotation
+        if (getImportTag() != null) {
+            item.getAnnotations().add(new InteractorAnnotation(IntactUtils.createMITopic("remark-internal", null),
+                    getImportTag()));
+        }else{
+            errorWriter.write("Could not add Job ID annotation because Job ID could not be retrieved");
+        }
         return item;
     }
 
@@ -106,4 +119,14 @@ public class IntactComplexDataAdditionProcessor implements ItemProcessor<Complex
     public void setErrorWriter(Writer errorWriter) {
         this.errorWriter = errorWriter;
     }
+
+    public String getImportTag() {
+        return importTag;
+    }
+
+    public void setImportTag(String importTag) {
+        this.importTag = importTag;
+    }
+
+
 }
