8e21f328f45c621ea1a87291fda69c4fbb358e80
Susie <hugetsusie@gmail.com>
2024-07-26 17:57:37 +0100

    Adding of the bash file for launching (still has some bugs to fix)

--- intact-orthology-import/src/main/java/uk/ac/ebi/intact/ortholog/OrthologsManager.java
+++ intact-orthology-import/src/main/java/uk/ac/ebi/intact/ortholog/OrthologsManager.java
@@ -1,36 +1,45 @@
 package uk.ac.ebi.intact.ortholog;
 
 import lombok.RequiredArgsConstructor;
+import org.hibernate.jpa.boot.internal.EntityManagerFactoryBuilderImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import psidev.psi.mi.jami.bridges.exception.BridgeFailedException;
-import psidev.psi.mi.jami.bridges.fetcher.InteractorFetcher;
-import psidev.psi.mi.jami.bridges.fetcher.ProteinFetcher;
-import psidev.psi.mi.jami.bridges.uniprot.UniprotProteinFetcher;
+import uk.ac.ebi.intact.jami.dao.IntactDao;
+import uk.ac.ebi.intact.jami.dao.impl.IntactDaoImpl;
+import uk.ac.ebi.intact.jami.model.extension.IntactProtein;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import java.io.*;
 import java.util.*;
+
 
 @RequiredArgsConstructor
 public class OrthologsManager {
 
-    UniprotProteinFetcher uniprotProteinFetcher;
-
-    public OrthologsManager(UniprotProteinFetcher uniprotProteinFetcher) {
-        this.uniprotProteinFetcher = new UniprotProteinFetcher();
-    }
+    private final IntactDao intactDao;
 
     public static void main(String[] args) throws IOException, BridgeFailedException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:/META-INF/orthology-import-spring.xml");
+        // Retrieve beans and use them
+        OrthologsFileReader orthologsFileReader = context.getBean("orthologsFileReader", OrthologsFileReader.class);
+        OrthologsFileParser orthologsFileParser = context.getBean("orthologsFileParser", OrthologsFileParser.class);
+        OrthologsProteinAssociation orthologsProteinAssociation = context.getBean("orthologsProteinAssociation", OrthologsProteinAssociation.class);
+
+
         String filePath = "orthologsData.txt";
         String urlPanther = "http://data.pantherdb.org/ftp/ortholog/current_release/AllOrthologs.tar.gz";
 
-        OrthologsManager orthologsManager = new OrthologsManager();
+//        OrthologsFileReader.decompressGzip(urlPanther,filePath); // working ✅
+//        Map<String, String> uniprotAndPanther = OrthologsFileParser.parseFile(filePath); // working ✅
 
-//        OrthologsFileReader.decompressGzip(urlPanther,filePath);
-//        Map<String, String> uniprotAndPanther = OrthologsFileParser.parseFile(filePath);
+//        OrthologsProteinAssociation orthologsProteinAssociation = new OrthologsProteinAssociation(new IntactDaoImpl());
+//        List<IntactProtein> proteins = orthologsProteinAssociation.getIntactProtein();
+        List<IntactProtein> prot = orthologsProteinAssociation.getIntactProtein();
 
-//        System.out.println(orthologsManager.uniprotProteinFetcher.fetchByIdentifier("P77650")); // return null
-
-//
-//        OrthologsProteinAssociation orthologsProteinAssociation = new OrthologsProteinAssociation(new UniprotProteinFetcher());
-//        orthologsProteinAssociation.getProtein();
     }
 }
 
