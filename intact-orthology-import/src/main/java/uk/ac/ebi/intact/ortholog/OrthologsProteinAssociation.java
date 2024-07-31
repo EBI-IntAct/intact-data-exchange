f111ef7e508c2c4aab9ffed4487f798cda7f18cb
Susie <hugetsusie@gmail.com>
2024-07-31 16:58:38 +0100

    Creation of a map containing IntactProtein and pantherID ok

--- intact-orthology-import/src/main/java/uk/ac/ebi/intact/ortholog/OrthologsProteinAssociation.java
+++ intact-orthology-import/src/main/java/uk/ac/ebi/intact/ortholog/OrthologsProteinAssociation.java
@@ -5,8 +5,6 @@ import uk.ac.ebi.intact.jami.model.extension.IntactProtein;
 import javax.annotation.Resource;
 import java.util.*;
 
-
-
 public class OrthologsProteinAssociation {
 
     @Resource(name="intactDao")
@@ -16,29 +14,41 @@ public class OrthologsProteinAssociation {
         this.intactDao = intactDao;
     }
 
-    public Collection<IntactProtein> getIntactProtein(){
-
-        //TODO: see if it is possible to fetch by UniprotKB to not have to fetch the whole db everytime
-
-        System.out.println("Fetching Intact Proteins...");
-        return intactDao.getProteinDao().getAll();
-    }
-
-    public Map<IntactProtein, String> associateProteinToPantherIndex(Map<String, String> uniprotIdAndPanther, Collection<IntactProtein> intactProteins) {
-        System.out.println("Associating Intact proteins to Panther identifier...");
-        Map<IntactProtein, String> intactProteinAndPanther = new HashMap<>();
-        for (Map.Entry<String, String> entry : uniprotIdAndPanther.entrySet()){
-            System.out.println("PANTHER " + entry.getKey() + ": " + entry.getValue());
-            for (Iterator<IntactProtein> iterator = intactProteins.iterator(); iterator.hasNext();){
-                //TODO see why the uniprotKB cannot be used
-                System.out.println(iterator.next().getUniprotkb());
-                if (entry.getValue().equals(iterator.next().getUniprotkb())){
-                    intactProteinAndPanther.put(iterator.next(), entry.getValue());
-                    System.out.println(iterator.next() + " -> " + entry.getValue());
-                }
+    public Map<Collection <IntactProtein>, String> associateProteinToPantherId(Map<String,String> uniprotIdAndPanther) {
+        Map<Collection <IntactProtein>, String> intactProteinAndPanther = new HashMap<>();
+        for (Map.Entry<String,String> entry : uniprotIdAndPanther.entrySet()) {
+            Collection <IntactProtein> intactProtein = this.intactDao.getProteinDao().getByXref(entry.getKey());
+            if (!intactProtein.isEmpty()) {
+                intactProteinAndPanther.put(intactProtein, entry.getValue());
             }
         }
-        System.out.println("Intact proteins associated to Panther identifier.");
         return intactProteinAndPanther;
     }
+
+//    public Collection<IntactProtein> getIntactProtein(){
+//        System.out.println("Fetching Intact Proteins...");
+//        return intactDao.getProteinDao().getAll();
+//    }
+
+//    public Map<IntactProtein, String> associateAllProteinsToPantherId(Map<String, String> uniprotIdAndPanther, Collection<IntactProtein> intactProteins) {
+//        System.out.println("Associating Intact proteins to Panther identifier...");
+//        Map<IntactProtein, String> intactProteinAndPanther = new HashMap<>();
+//            for (Iterator<IntactProtein> iterator = intactProteins.iterator(); iterator.hasNext();){
+//                System.out.println(iterator.next().getUniprotkb());
+//                System.out.println(iterator.next().getIdentifiers());
+//                System.out.println(uniprotIdAndPanther.get(iterator.next().getUniprotkb()));
+//                if (uniprotIdAndPanther.get(iterator.next().getUniprotkb()) != null){
+//                    intactProteinAndPanther.put(iterator.next(),uniprotIdAndPanther.get(iterator.next().getUniprotkb()));
+//                    System.out.println(iterator.next() + " -> " + uniprotIdAndPanther.get(iterator.next().getUniprotkb()));
+//                }
+//
+//                System.out.println(iterator.next().getUniprotkb());
+//                if (entry.getValue().equals(iterator.next().getUniprotkb())){
+//                    intactProteinAndPanther.put(iterator.next(), entry.getValue());
+//                    System.out.println(iterator.next() + " -> " + entry.getValue());
+//                }
+//            }
+//        System.out.println("Intact proteins associated to Panther identifier.");
+//        return intactProteinAndPanther;
+//    }
 }
