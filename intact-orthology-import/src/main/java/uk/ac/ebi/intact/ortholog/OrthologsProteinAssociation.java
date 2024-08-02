54d5ff754ce8f21b2ee95adbdeeb481680e19cb7
Susie <hugetsusie@gmail.com>
2024-08-02 16:45:19 +0100

    Adding of panther xref to intact protein OK; (TO SEE for the panther db id)

--- intact-orthology-import/src/main/java/uk/ac/ebi/intact/ortholog/OrthologsProteinAssociation.java
+++ intact-orthology-import/src/main/java/uk/ac/ebi/intact/ortholog/OrthologsProteinAssociation.java
@@ -2,10 +2,14 @@ package uk.ac.ebi.intact.ortholog;
 
 import uk.ac.ebi.intact.jami.dao.IntactDao;
 import uk.ac.ebi.intact.jami.model.extension.IntactProtein;
+
 import javax.annotation.Resource;
 import java.util.*;
+import java.util.logging.Logger;
 
 public class OrthologsProteinAssociation {
+
+    private static final Logger logger = Logger.getLogger(OrthologsProteinAssociation.class.getName());
 
     @Resource(name="intactDao")
     private final IntactDao intactDao;
@@ -14,33 +18,43 @@ public class OrthologsProteinAssociation {
         this.intactDao = intactDao;
     }
 
-//    public Map<Collection <IntactProtein>, String> associateProteinToPantherId(Map<String,String> uniprotIdAndPanther) {
-//        Map<Collection <IntactProtein>, String> intactProteinAndPanther = new HashMap<>();
-//        for (Map.Entry<String,String> entry : uniprotIdAndPanther.entrySet()) {
-//            Collection <IntactProtein> intactProtein = this.intactDao.getProteinDao().getByXref(entry.getKey());
-//            if (!intactProtein.isEmpty()) {
-//                intactProteinAndPanther.put(intactProtein, entry.getValue());
-//            }
-//        }
-//        return intactProteinAndPanther;
-//    }
-
     public Collection<IntactProtein> getIntactProtein(){
-        System.out.println("Fetching Intact Proteins...");
+        logger.info("Fetching all Intact Proteins...");
         return intactDao.getProteinDao().getAll();
     }
 
+    public Collection<IntactProtein> flattenProteinCollection(Collection<Collection<IntactProtein>> nestedProteins) {
+        List<IntactProtein> flatProteins = new ArrayList<>();
+        for (Collection<IntactProtein> proteinCollection : nestedProteins) {
+            flatProteins.addAll(proteinCollection);
+        }
+        return flatProteins;
+    }
+
+    public Collection<IntactProtein> getFewIntactProtein() {
+        logger.info("Fetching few Intact Proteins...");
+        List<Collection<IntactProtein>> fewIntactProteins = new ArrayList<>();
+        List<String> proteinsToFetch = Arrays.asList("P38153", "Q01217", "P38116", "P32449", "Q12406", "P43561", "P15646", "P43561", "P17710", "P19659");
+
+        var proteinDao = intactDao.getProteinDao();
+        for (String proteinToFetch : proteinsToFetch) {
+            fewIntactProteins.add(proteinDao.getByXref(proteinToFetch));
+        }
+        return flattenProteinCollection(fewIntactProteins);
+    }
+
     public Map<IntactProtein, String> associateAllProteinsToPantherId(Map<String, String> uniprotIdAndPanther, Collection<IntactProtein> intactProteins) {
-        System.out.println("Associating Intact proteins to Panther identifier...");
+        logger.info("Associating Intact proteins to Panther identifier...");
         Map<IntactProtein, String> intactProteinAndPanther = new HashMap<>();
-            for (Iterator<IntactProtein> iterator = intactProteins.iterator(); iterator.hasNext();){
-                System.out.println(iterator.next().getUniprotkb());
-                if (uniprotIdAndPanther.get(iterator.next().getUniprotkb()) != null){
-                    intactProteinAndPanther.put(iterator.next(),uniprotIdAndPanther.get(iterator.next().getUniprotkb()));
-                    System.out.println(iterator.next() + " -> " + uniprotIdAndPanther.get(iterator.next().getUniprotkb()));
-                }
+
+        for (IntactProtein protein : intactProteins) {
+            String pantherId = uniprotIdAndPanther.get(protein.getUniprotkb());
+            if (pantherId != null) {
+                intactProteinAndPanther.put(protein, pantherId);
+                System.out.println(protein.getUniprotkb() + " -> " + pantherId);
             }
-        System.out.println("Intact proteins associated to Panther identifier.");
+        }
+        logger.info("Intact proteins associated to Panther identifier.");
         return intactProteinAndPanther;
     }
 }
