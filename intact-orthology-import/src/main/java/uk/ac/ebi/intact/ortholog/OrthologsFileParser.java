e9453bd9b38b693a7664a37453eeca07629663ef
Susie <hugetsusie@gmail.com>
2024-08-06 14:02:24 +0100

    adding of loggers instead of system.out

--- intact-orthology-import/src/main/java/uk/ac/ebi/intact/ortholog/OrthologsFileParser.java
+++ intact-orthology-import/src/main/java/uk/ac/ebi/intact/ortholog/OrthologsFileParser.java
@@ -3,22 +3,23 @@ package uk.ac.ebi.intact.ortholog;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class OrthologsFileParser {
 
+    private static final Logger log = Logger.getLogger(OrthologsFileParser.class.getName());
+
     public static Map<String, String> parseFile(String filePath) {
         Pattern uniprotKBRegex = Pattern.compile("UniProtKB=([A-Z0-9]+)");
         Pattern pantherRegex = Pattern.compile("PTHR\\d+");
         Map<String, String> uniprotAndPTHR = new HashMap<>();
-        Instant startTime = Instant.now();
-        System.out.println("Parsing file...");
+        log.info("Parsing file...");
 
         try (
                 BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
@@ -41,8 +42,8 @@ public class OrthologsFileParser {
             throw new RuntimeException(e);
         }
         Instant endTime = Instant.now();
-        System.out.println("File parsed");
-        System.out.println("Processing time:" + Duration.between(startTime, endTime));
+        log.info("File parsed.");
+        log.info("Number of Panther identifiers: " + uniprotAndPTHR.size());
         return uniprotAndPTHR;
     }
 }
--- intact-orthology-import/src/main/java/uk/ac/ebi/intact/ortholog/OrthologsFileReader.java
+++ intact-orthology-import/src/main/java/uk/ac/ebi/intact/ortholog/OrthologsFileReader.java
@@ -6,24 +6,24 @@ import org.apache.commons.io.IOUtils;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.time.Duration;
-import java.time.Instant;
+import java.util.logging.Logger;
 import java.util.zip.GZIPInputStream;
 
 public class OrthologsFileReader{
+
+    private static final Logger log = Logger.getLogger(OrthologsFileReader.class.getName());
 
     public static void decompressGzip(String url, String filePath) throws IOException {
         URL gzipUrl = new URL(url);
         HttpURLConnection connection = (HttpURLConnection) gzipUrl.openConnection();
         int responseCode = connection.getResponseCode();
-        Instant startTime = Instant.now();
 
         if (responseCode == HttpURLConnection.HTTP_OK) {
-            System.out.println("Connected to URL.");
+            log.info("Connected to URL.");
             try (InputStream in = connection.getInputStream();
                  GZIPInputStream gis = new GZIPInputStream(in);
                  TarArchiveInputStream tis = new TarArchiveInputStream(gis)) {
-                System.out.println("Decompressing...");
+                log.info("Decompressing...");
                 while (tis.getNextTarEntry() != null) {
                     File outputFile = new File(filePath);
                     try (FileOutputStream fos = new FileOutputStream(outputFile, false)) {
@@ -31,17 +31,15 @@ public class OrthologsFileReader{
                         IOUtils.copy(tis, fos);
                     }
                 }
-                System.out.println("File decompressed, data in " + filePath);
-                Instant endTime = Instant.now();
-                System.out.println("Processing time:" + Duration.between(startTime, endTime));
+                log.info("File decompressed, data in " + filePath);
             }
             finally {
                 connection.disconnect();
-                System.out.println("Disconnected from URL.");
+                log.info("Disconnected from URL.");
             }
         }
         else {
-            System.out.println("GZIP returned unexpected response: " + responseCode);
+            log.info("GZIP returned unexpected response: " + responseCode);
         }
     }
 }
