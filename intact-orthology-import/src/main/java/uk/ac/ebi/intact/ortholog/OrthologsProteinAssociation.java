57330b2cf334158c9c613790f37d581bce38d574
Susie <hugetsusie@gmail.com>
2024-08-08 16:54:25 +0100

    Adding saving of last parsed panther index + loading from this saving

--- intact-orthology-import/src/main/java/uk/ac/ebi/intact/ortholog/OrthologsProteinAssociation.java
+++ intact-orthology-import/src/main/java/uk/ac/ebi/intact/ortholog/OrthologsProteinAssociation.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j;
 import uk.ac.ebi.intact.jami.dao.IntactDao;
 import uk.ac.ebi.intact.jami.model.extension.IntactProtein;
 import javax.annotation.Resource;
+import java.io.*;
 import java.util.*;
 
 @Log4j
@@ -38,18 +39,65 @@ public class OrthologsProteinAssociation {
     public Map<IntactProtein, String> associateAllProteinsToPantherId(Map<String, String> uniprotIdAndPanther, Collection<IntactProtein> intactProteins) {
         log.info("Associating Intact proteins to Panther identifier...");
         Map<IntactProtein, String> intactProteinAndPanther = new HashMap<>();
+        List<IntactProtein> intactProteinList = new ArrayList<>(intactProteins);
 
         int counter = 0;
+        int index = 0;
+        String[] lastSave= fetchFromStopped();
 
-        for (IntactProtein protein : intactProteins) {
-            String pantherId = uniprotIdAndPanther.get(protein.getUniprotkb());
+        if (lastSave != null){
+            index = Integer.parseInt(lastSave[3]);
+            counter = Integer.parseInt(lastSave[2]);
+        }
+
+        for (int i = index; i < intactProteinList.size(); i++) {
+            IntactProtein protein = intactProteinList.get(i);
+            String proteinId = protein.getUniprotkb();
+            String pantherId = uniprotIdAndPanther.get(proteinId);
             if (pantherId != null) {
                 counter += 1;
-                intactProteinAndPanther.put(protein, pantherId);
-                System.out.println(protein.getUniprotkb() + " -> " + pantherId + " index = " + counter);
+//                intactProteinAndPanther.put(protein, pantherId);
+                System.out.println(proteinId + " -> " + pantherId + " index = " + counter);
+                dataWriter(proteinId + "," + pantherId + "," + counter + "," + i + "\n");
             }
         }
-        log.info("Number of protein associated to Panther identifier: " + intactProteinAndPanther.size());
+        log.info("Number of protein associated to Panther identifier: " + counter);
         return intactProteinAndPanther;
+    }
+
+    public String[] fetchFromStopped() {
+     String line = dataReader();
+     if (line != null) {
+         return line.split(",");
+     }
+     return null;
+    }
+
+    public static String dataReader() {
+        String lastLine = null;
+        try (BufferedReader reader = new BufferedReader(new FileReader("proteinAndPanther.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 4) {
+                    lastLine = line;
+                }
+            }
+            return lastLine;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void dataWriter(String toWrite){
+        try {
+            FileWriter fileWriter = new FileWriter("proteinAndPanther.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(toWrite);
+            bufferedWriter.close();
+        }
+        catch (Exception e) {
+            e.getStackTrace();
+        }
     }
 }
